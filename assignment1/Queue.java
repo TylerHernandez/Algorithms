618b04ba35f0a640be63743406a8bfd4c5728e09
Tyler Hernandez <tylerh6201@gmail.com>
2022-09-09 13:45:07 -0400

    Adds Queue class
    Adds queue class template

---
+++ assignment1/Queue.java
@@ -0,0 +1,20 @@
+class Queue {
+
+    private Node head;
+    private Node tail;
+
+    public Queue(){
+        //Node first_node = new Node();
+
+    }
+
+    public void enqueue(Node node){
+        
+    }
+    public Node Dequeue(){
+        return new Node();
+    }
+    public boolean isEmpty(){
+        return true;
+    }
+}

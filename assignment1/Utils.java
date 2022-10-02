287017d32fc4f4a615f26ada253b3ace39f0bd81
Tyler Hernandez <tylerh6201@gmail.com>
2022-10-02 16:42:48 -0400

    Adds printarray to utils.
    decided this was better off in utils, so i can use it in other assignments.

--- assignment1/Utils.java
+++ assignment1/Utils.java
@@ -21,4 +21,38 @@ class Utils {
         System.arraycopy(originalArray, 0, newArray, 0, originalArray.length - 1);
         return newArray;
     }
+
+    // I kept wanting to see the output of the array to make sure it was sorted and
+    // kept getting
+    // "[I@18b4aac2", hence the creation of this function.
+    public static void printArray(int[] array) {
+        if (array.length == 0) {
+            System.out.println("[ ]");
+            System.out.println();
+        } else {
+        System.out.print("[ ");
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print((char) array[i] + ", ");
+        }
+        System.out.print(array[array.length - 1]);
+        System.out.println("]");
+        System.out.println();
+    }
+    }
+
+    public static void printArray(char[] array) {
+        if (array.length == 0) {
+            System.out.println("[ ]");
+            System.out.println();
+        } else {
+            System.out.print("[ ");
+            for (int i = 0; i < array.length - 1; i++) {
+                System.out.print(array[i] + ", ");
+            }
+            System.out.print(array[array.length - 1]);
+            System.out.println(" ]");
+            System.out.println();
+        }
+    }
+
 }

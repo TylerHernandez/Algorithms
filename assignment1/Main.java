a08197fa7a6f048eea29e138943da3f4925340ee
Tyler Hernandez <tylerh6201@gmail.com>
2022-10-19 12:58:51 -0400

    Refactoring

--- assignment1/Main.java
+++ assignment1/Main.java
@@ -1,72 +1,72 @@
-class Main {
-    public static void main(String[] args) throws Exception {
-        // 12 Palindromes in test cases.
-        Stack stack = new Stack();
+// class Main {
+//     public static void main(String[] args) throws Exception {
+//         // 12 Palindromes in test cases.
+//         Stack stack = new Stack();
 
-        Queue queue = new Queue();
+//         Queue queue = new Queue();
 
-        Reader reader = new Reader("INSERT_DIRECTORY_HERE", 0);
+//         Reader reader = new Reader("INSERT_DIRECTORY_HERE", 0);
 
-        int i = 0; // Will hold char as an integer.
+//         int i = 0; // Will hold char as an integer.
 
-        // Holds each line 
-        int[] line = reader.getNextLine();
+//         // Holds each line 
+//         int[] line = reader.getNextLine();
 
-        // Until i've reached the end of the file, keep looping
-        while (i != -1) {
+//         // Until i've reached the end of the file, keep looping
+//         while (i != -1) {
 
-            // Loop through line, add each char to stack and queue.
-            for (int x = 0; x < line.length; x++) {
-                char ch = Character.toUpperCase((char) line[x]);
-                if ((ch != ' ') && (ch != ',') && (ch != '.') && (ch != '\'') && (ch != '\n')) {
-                    stack.push(new Node(ch));
-                    queue.enqueue(new Node(ch));
-                }
+//             // Loop through line, add each char to stack and queue.
+//             for (int x = 0; x < line.length; x++) {
+//                 char ch = Character.toUpperCase((char) line[x]);
+//                 if ((ch != ' ') && (ch != ',') && (ch != '.') && (ch != '\'') && (ch != '\n')) {
+//                     stack.push(new Node(ch));
+//                     queue.enqueue(new Node(ch));
+//                 }
 
-            }
+//             }
 
-            isPalindrome(stack, queue);
+//             isPalindrome(stack, queue);
 
-            // Reset queue and stack.
-            queue = new Queue();
-            stack = new Stack();
+//             // Reset queue and stack.
+//             queue = new Queue();
+//             stack = new Stack();
 
-            // Grab the next line
-            line = reader.getNextLine();
-            i = line[0];
+//             // Grab the next line
+//             line = reader.getNextLine();
+//             i = line[0];
 
-        } // ends while
+//         } // ends while
 
-    }
+//     }
 
-    // Prints out palindromes found and returns boolean isPalindrome.
-    public static Boolean isPalindrome(Stack stack, Queue queue) {
-        // If both are empty, return true. If only one is empty, return false.
-        if (stack.isEmpty() && queue.isEmpty()) {
-            System.out.println("");
-            return true;
-        } else if (stack.isEmpty() && !queue.isEmpty()) {
-            return false;
-        } else if (!stack.isEmpty() && queue.isEmpty()) {
-            return false;
-        }
+//     // Prints out palindromes found and returns boolean isPalindrome.
+//     public static Boolean isPalindrome(Stack stack, Queue queue) {
+//         // If both are empty, return true. If only one is empty, return false.
+//         if (stack.isEmpty() && queue.isEmpty()) {
+//             System.out.println("");
+//             return true;
+//         } else if (stack.isEmpty() && !queue.isEmpty()) {
+//             return false;
+//         } else if (!stack.isEmpty() && queue.isEmpty()) {
+//             return false;
+//         }
 
-        // Stack length is constantly changing, so record length first.
-        int length = stack.length;
+//         // Stack length is constantly changing, so record length first.
+//         int length = stack.length;
 
-        // String to print out if it is a palindrome.
-        String str = "";
+//         // String to print out if it is a palindrome.
+//         String str = "";
 
-        for (int i = 0; i < length; i++) {
-            str += queue.getHead().getData();
+//         for (int i = 0; i < length; i++) {
+//             str += queue.getHead().getData();
 
-            // Compare each Node's data. If they do not match, return false.
-            if (stack.pop().getData() != queue.dequeue().getData()) {
-                return false;
-            }
-        }
-        System.out.println(str);
-        return true;
-    }
+//             // Compare each Node's data. If they do not match, return false.
+//             if (stack.pop().getData() != queue.dequeue().getData()) {
+//                 return false;
+//             }
+//         }
+//         System.out.println(str);
+//         return true;
+//     }
 
-}
+// }
--- assignment2/Main.java
+++ assignment2/Main.java
@@ -1,119 +1,119 @@
 
-class Main {
+// class Main {
 
-    static int quickCount = 0;
-    static int selectionCount = 0;
-    static int insertionCount = 0;
+//     static int quickCount = 0;
+//     static int selectionCount = 0;
+//     static int insertionCount = 0;
 
-    // Driver for Assignment 2.
-    public static void main(String[] args) throws Exception {
-        Reader reader = new Reader("./magicitems.txt");
-        // I know you said to just put "magicitems.txt", but that was not working
-        // regardless of whatever directory I inserted the file into.
+//     // Driver for Assignment 2.
+//     public static void main(String[] args) throws Exception {
+//         Reader reader = new Reader("./magicitems.txt");
+//         // I know you said to just put "magicitems.txt", but that was not working
+//         // regardless of whatever directory I inserted the file into.
 
-        // Characters we will ignore when reading from the file.
-        char[] ignoreList = { ' ', ',', '.', '\'', '-', '+' };
+//         // Characters we will ignore when reading from the file.
+//         char[] ignoreList = { ' ', ',', '.', '\'', '-', '+' };
 
-        // Holds each char of a line
-        char[] line = reader.getNextLineOfChars(ignoreList, true);
+//         // Holds each char of a line
+//         char[] line = reader.getNextLineOfChars(ignoreList, true);
 
-        // Holds each line as a string
-        String[] fullText = new String[0]; // Start the array at no length in case given an empty list.
+//         // Holds each line as a string
+//         String[] fullText = new String[0]; // Start the array at no length in case given an empty list.
 
-        // Until we've reached the end of the file, keep looping.
-        // Even empty lines in the file will have at least a '\n' character.
-        while (line.length > 0) {
-            // Though, we must ignore '\n' characters manually in order to keep looping
-            // based on if there is a character in the next line.
-            if (line[line.length - 1] == '\n') {
-                // it seems that '\n' characters only show up at the end of the array (and not
-                // in the last line).
-                line = Utils.removeLastElementOfArray(line);
-            }
+//         // Until we've reached the end of the file, keep looping.
+//         // Even empty lines in the file will have at least a '\n' character.
+//         while (line.length > 0) {
+//             // Though, we must ignore '\n' characters manually in order to keep looping
+//             // based on if there is a character in the next line.
+//             if (line[line.length - 1] == '\n') {
+//                 // it seems that '\n' characters only show up at the end of the array (and not
+//                 // in the last line).
+//                 line = Utils.removeLastElementOfArray(line);
+//             }
 
-            // Expand array by one everytime we add a new value to it.
-            fullText = Utils.expandArrayByOne(fullText);
-            // Takes line of characters and puts them into fullText as a concatted string.
-            fullText[fullText.length - 1] = String.valueOf(line);
+//             // Expand array by one everytime we add a new value to it.
+//             fullText = Utils.expandArrayByOne(fullText);
+//             // Takes line of characters and puts them into fullText as a concatted string.
+//             fullText[fullText.length - 1] = String.valueOf(line);
 
-            // Grab the next line
-            line = reader.getNextLineOfChars(ignoreList, true);
+//             // Grab the next line
+//             line = reader.getNextLineOfChars(ignoreList, true);
 
-        } // ends while
+//         } // ends while
 
-        String[] ORIGINAL_TEXT = fullText;
+//         String[] ORIGINAL_TEXT = fullText;
 
-        // These sorts will return the amount of comparisons they performed.
+//         // These sorts will return the amount of comparisons they performed.
 
-        // Insertion Sort!
-        insertionCount += insertionSort(fullText);
-        fullText = ORIGINAL_TEXT;
+//         // Insertion Sort!
+//         insertionCount += insertionSort(fullText);
+//         fullText = ORIGINAL_TEXT;
 
-        // Selection Sort!
-        selectionCount += selectionSort(fullText);
-        fullText = ORIGINAL_TEXT;
+//         // Selection Sort!
+//         selectionCount += selectionSort(fullText);
+//         fullText = ORIGINAL_TEXT;
 
-        // Recursive functions in java are trickier when it comes to returning values,
-        // soooo... global variables.
+//         // Recursive functions in java are trickier when it comes to returning values,
+//         // soooo... global variables.
 
-        // Merge Sort!
-        MergeSort msort = new MergeSort();
-        msort.sort(fullText, 0, fullText.length - 1);
-        fullText = ORIGINAL_TEXT;
+//         // Merge Sort!
+//         MergeSort msort = new MergeSort();
+//         msort.sort(fullText, 0, fullText.length - 1);
+//         fullText = ORIGINAL_TEXT;
 
-        // Quick Sort!
-        QuickSort qsort = new QuickSort();
-        qsort.sort(fullText, 0, fullText.length - 1);
-        Utils.printArray(fullText);
-        fullText = ORIGINAL_TEXT;
+//         // Quick Sort!
+//         QuickSort qsort = new QuickSort();
+//         qsort.sort(fullText, 0, fullText.length - 1);
+//         Utils.printArray(fullText);
+//         fullText = ORIGINAL_TEXT;
 
-        System.out.println("Insertion sort: " + insertionCount);
-        System.out.println("Selection sort: " + selectionCount);
-        System.out.println("Merge sort: " + msort.mergeCount);
-        System.out.println("Quick sort: " + qsort.quickCount);
+//         System.out.println("Insertion sort: " + insertionCount);
+//         System.out.println("Selection sort: " + selectionCount);
+//         System.out.println("Merge sort: " + msort.mergeCount);
+//         System.out.println("Quick sort: " + qsort.quickCount);
 
-    }
+//     }
 
-    // Without Recursion
-    public static int selectionSort(String[] line) {
-        int recordedComparisons = 0;
+//     // Without Recursion
+//     public static int selectionSort(String[] line) {
+//         int recordedComparisons = 0;
 
-        // loop over entire array with ptr1.
-        for (int ptr1 = 0; ptr1 < line.length - 1; ptr1++) {
+//         // loop over entire array with ptr1.
+//         for (int ptr1 = 0; ptr1 < line.length - 1; ptr1++) {
 
-            // Retrieve index of the earliest alphabetical character in the subarray.
-            int minimum = ptr1;
-            for (int i = ptr1 + 1; i < line.length; i++) {
-                if (line[i].compareTo(line[minimum]) < 0) {
-                    minimum = i;
-                }
-                recordedComparisons++;
-            }
+//             // Retrieve index of the earliest alphabetical character in the subarray.
+//             int minimum = ptr1;
+//             for (int i = ptr1 + 1; i < line.length; i++) {
+//                 if (line[i].compareTo(line[minimum]) < 0) {
+//                     minimum = i;
+//                 }
+//                 recordedComparisons++;
+//             }
 
-            // Swap the found minimum element with line[ptr1].
-            String temp = line[minimum]; // record before writing over.
-            line[minimum] = line[ptr1];
-            line[ptr1] = temp;
-        }
-        return recordedComparisons;
-    }
+//             // Swap the found minimum element with line[ptr1].
+//             String temp = line[minimum]; // record before writing over.
+//             line[minimum] = line[ptr1];
+//             line[ptr1] = temp;
+//         }
+//         return recordedComparisons;
+//     }
 
-    // Insertion Sort! (does not use recursion).
-    public static int insertionSort(String[] line) {
-        int recordedComparisons = 0;
-        for (int ptr1 = 1; ptr1 < line.length; ptr1++) { // we do not need to compare the first index.
-            String key = line[ptr1]; // record our character to copy over.
-            int ptr2 = ptr1 - 1;
+//     // Insertion Sort! (does not use recursion).
+//     public static int insertionSort(String[] line) {
+//         int recordedComparisons = 0;
+//         for (int ptr1 = 1; ptr1 < line.length; ptr1++) { // we do not need to compare the first index.
+//             String key = line[ptr1]; // record our character to copy over.
+//             int ptr2 = ptr1 - 1;
 
-            // Moves over every character greater than our key by one.
-            while (ptr2 >= 0 && line[ptr2].compareTo(key) >= 0) { // this counts as two comparisons.
-                recordedComparisons++;
-                line[ptr2 + 1] = line[ptr2];
-                ptr2--;
-            }
-            line[ptr2 + 1] = key; // paste over (insert) our recorded character.
-        }
-        return recordedComparisons;
-    }
+//             // Moves over every character greater than our key by one.
+//             while (ptr2 >= 0 && line[ptr2].compareTo(key) >= 0) { // this counts as two comparisons.
+//                 recordedComparisons++;
+//                 line[ptr2 + 1] = line[ptr2];
+//                 ptr2--;
+//             }
+//             line[ptr2 + 1] = key; // paste over (insert) our recorded character.
+//         }
+//         return recordedComparisons;
+//     }
 
-}
+// }

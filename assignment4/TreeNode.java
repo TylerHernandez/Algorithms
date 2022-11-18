61d8ee8d5adf9508972d8a11928ea8b353460831
Tyler Hernandez <tylerh6201@gmail.com>
2022-11-17 19:08:20 -0500

    Adds TreeNode class

---
+++ assignment4/TreeNode.java
@@ -0,0 +1,17 @@
+public class TreeNode {
+
+    String value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(String value) {
+        this.value = value;
+        left = null;
+        right = null;
+    }
+
+    public void insert(TreeNode treeNode) {
+        if (treeNode.value )
+    }
+
+}
